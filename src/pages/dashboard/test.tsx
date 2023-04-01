f156d6b58a7f2bcb26bcbeff6c43af13857544cc
Akhilendra <akhilendra1604@gmail.com>
2023-04-01 10:20:41 +0530

    .

--- src/pages/dashboard/test.tsx
+++ src/pages/dashboard/test.tsx
@@ -58,7 +58,7 @@ const Test: React.FC<ComponentProps> = ({
 	React.useEffect(() => {
 		const timer = setInterval(() => {
 			const finaltime = new Date('April 01, 2023 9:30:00').getTime();
-			const endtime = new Date('April 01, 2023 10:00:00').getTime();
+			const endtime = new Date('April 01, 2023 10:30:00').getTime();
 			let currtime = new Date().getTime();
 			let timeleft = finaltime - currtime;
 			console.log(timeleft);
